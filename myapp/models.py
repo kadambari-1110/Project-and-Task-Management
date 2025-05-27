3b74689c2685dfa1420887976e62b0465e379a14
kadambari-1110 <kadambari.wale13@gmail.com>
2025-05-27 19:36:02 +0530

    models added

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class project(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    # team_lead = models.ForeignKey()
+    deadline = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+
+class task(models.Model):
+    project = models.ForeignKey(project,on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+    # members = models.CharField()
+    status = models.BooleanField(choices=[('Pending','Pending'),('Completed','Completed')])
+    deadline = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
