5b88c86ab4132877d3957b66d32cc74ff18cd7d5
kadambari-1110 <kadambari.wale13@gmail.com>
2025-05-28 20:16:36 +0530

    model project and task created

--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class project(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
-    # team_lead = models.ForeignKey()
+    team_lead = models.ForeignKey(User,related_name="teamleader",on_delete=models.CASCADE)
     deadline = models.DateField()
 
     def __str__(self):
@@ -13,11 +14,11 @@ class project(models.Model):
     
 
 class task(models.Model):
-    project = models.ForeignKey(project,on_delete=models.CASCADE)
+    project = models.ForeignKey(project,on_delete=models.CASCADE,related_name="tasks")
     name = models.CharField(max_length=20)
-    description = models.CharField(max_length=100)
-    # members = models.CharField()
-    status = models.BooleanField(choices=[('Pending','Pending'),('Completed','Completed')])
+    description = models.CharField(max_length=100)  
+    members = models.ForeignKey(User,on_delete=models.CASCADE,related_name="member")
+    status = models.CharField(choices=[('Pending','Pending'),('Completed','Completed')])
     deadline = models.DateTimeField()
 
     def __str__(self):
